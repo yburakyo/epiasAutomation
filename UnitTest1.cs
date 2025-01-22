2fb43b91754b0fc1c3ee1b8a8a8609ea2cad13b7
yburakyo <yusufburakyozgat@gmail.com>
2025-01-22 14:27:48 +0300

    basic script added

---
+++ UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace epiasAutomation;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl("https://seffaflik-prp.epias.com.tr/home");
+
+        System.Threading.Thread.Sleep(2000); // sleep to see the website, will remove later
+        driver.Quit();
+        
+    }
+}
